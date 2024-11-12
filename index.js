1ff77efbb3fe66fea2cdbd4654b1552b57a89e99
Agrapujya Lashkari <joebrion20@gmail.com>
2024-11-12 11:45:56 +0530

    changed the cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-import cors from "cors"
+// import cors from "cors"
 dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(
     ()=>{
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI).then(
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+// app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
 
 import authRouter from './routes/auth.route.js'
 import noteRouter from './routes/note.route.js'
